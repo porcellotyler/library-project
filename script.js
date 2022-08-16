c5aa3c2a4b6fee1868d6fa5eedc36c2816fd944a
porcellotyler <porcellotyler@gmail.com>
2022-08-15 20:39:04 -0400

    Add 'new book' button pushing results to table

--- script.js
+++ script.js
@@ -14,34 +14,47 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
     console.log(myLibrary);
-    return
+
+    let tableBody = document.getElementById("tableBody");tableBody.innerHTML = "";
+
+    createTable(myLibrary);
+    return 
 }
 
 const nonfiction = new Book('memoir', 'ty pee', '253', 'read');
     addBookToLibrary(nonfiction);
 
-const fiction = new Book('a fake book', 'kurt vonnegut', '298', 'not read yet');
-    addBookToLibrary(fiction);
+function createTable(library){
+    for (let i = 0; i < library.length; i++) {
+        console.table(library);
 
-const poetry = new Book('a poem collection', 'a poet', '87', 'not read yet');
-    addBookToLibrary(poetry);
+        let table = document.getElementById("tableBody")
 
-for (let i = 0; i < myLibrary.length; i++) {
-    console.table(myLibrary);
+        let row = table.insertRow();
+        let title = row.insertCell(0);
+        title.innerText = library[i].title;
+        
+        let author = row.insertCell(1);
+        author.innerText = library[i].author;
 
-    let table = document.getElementById("tableBody")
+        let pages = row.insertCell(2);
+        pages.innerText = library[i].pages;
 
-    let row = table.insertRow();
-    let title = row.insertCell(0);
-    title.innerText = myLibrary[i].title;
-    
-    let author = row.insertCell(1);
-    author.innerText = myLibrary[i].author;
-
-    let pages = row.insertCell(2);
-    pages.innerText = myLibrary[i].pages;
-
-    let read = row.insertCell(3);
-    read.innerText = myLibrary[i].read;
-
+        let read = row.insertCell(3);
+        read.innerText = library[i].read;
+    };
 };
+
+const addBook = document.getElementById("newBookButton")
+
+addBook.onclick = function() {
+    let newBook = new Book((document.getElementById("title").value), (document.getElementById("author").value), (document.getElementById("pages").value), (document.getElementById("readYet").value)
+    );
+
+    document.getElementById("title").value = '';
+    document.getElementById("author").value = '';
+    document.getElementById("pages").value = '';
+    document.getElementById("readYet").value = '';
+
+    return addBookToLibrary(newBook)
+}
